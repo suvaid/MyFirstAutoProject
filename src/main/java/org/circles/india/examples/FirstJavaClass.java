e84346ecc46d9b5ebfbd34fd9d016efccb75c8b6
mohammed shamas c <mohammedshamas@yahoo.in>
2020-06-09 09:49:20 +0530

    FirstCommit

---
+++ src/main/java/org/circles/india/examples/FirstJavaClass.java
@@ -0,0 +1,8 @@
+package org.circles.india.examples;
+
+public class FirstJavaClass {
+    public static void main(String args[])
+    {
+        System.out.println("My first Automation project");
+    }
+}
